74cf013622baded8e3b2c4782dd56394f5143968
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2019-10-24 21:44:44 -0400

    Week 2 Project 1

---
+++ Assignments/FSW-105/Week 2/Pause and Practice/index.js
@@ -0,0 +1,18 @@
+if (5 > 3){
+    console.log("is greater than")
+}
+
+let cat = 3
+
+if (cat === 3){
+    console.log("is the length")
+} 
+
+if ('cat' !== 'dog'){
+    console.log("not the same")
+}
+
+var person = {
+    name: "Bobby",
+    age: 12
+}
