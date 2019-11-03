be31f980ef6d55fe613e01d9b42c23b2092e68dd
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2019-11-02 21:59:52 -0400

    Week 3 Pause and Practice

--- Assignments/FSW-110/Week 3/Pause and Practice 1/visualList.js
+++ Assignments/FSW-110/Week 3/Pause and Practice 1/visualList.js
@@ -10,18 +10,11 @@ for (i = 0; i < 8; i++) {
    var namesList = document.createElement("h1")
    namesList.innerHTML = names[i]
    document.body.appendChild(namesList);
+   namesList.style.textAlign = "center"
+   namesList.style.color = "red"
 }
 
 
-/*var h1Element = getElementsByTagName("h1");
-for (var z = 0; z < h1Elements.length; i++) {
-   h1Elements[z].style.color = "#fff";
-}
-
-var x = document.getElementById("body");
-for ( var z = 0; z <= x.length; z++) {
-   z.getElementsByTagName("H1")[1].style.backgroundColor = "red";
-}*/
 hello.style.textAlign = "center";  
 document.body.style.backgroundColor = "burlywood"
 
