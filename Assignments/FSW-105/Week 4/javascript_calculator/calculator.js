7c59289408f8d75bb3b41404089c86b39ab5f51c
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2019-11-10 19:14:17 -0500

    Javascript Calculator

--- Assignments/FSW-105/Week 4/javascript_calculator/calculator.js
+++ Assignments/FSW-105/Week 4/javascript_calculator/calculator.js
@@ -2,7 +2,7 @@ const { question } = require("readline-sync")
 
 let firstNumber = parseInt(question("Please enter your first number? "))
 let secondNumber = parseInt(question("Please enter addsecond number? "))
-let operation = question("Please enter the operation to perform: +, -, *, / ?")
+let operation = question("Please enter the operation to perform: add, sub, mul, div?")
 let num1 = firstNumber
 let num2 = secondNumber
 function sum(num1, num2){
@@ -24,7 +24,16 @@ function divide(num1, num2){
     return num1 / num2
 }
 //console.log(num1 / num2)
-if (operation = )
+
+if (operation == 'add'){
+    console.log("The result is: " + sum(num1, num2))
+} else if(operation == 'sub'){
+    console.log("The result is: " + subtract(num1, num2))
+} else if(operation == 'mul'){
+    console.log("The result is: " + multiply(num1, num2))
+} else if (operation == 'div'){
+    console.log("The result is: " + divide(num1, num2))
+}
 //console.log("The result is" + " " + num1, num2)
 
 /*const { question } = require('readline-sync')
