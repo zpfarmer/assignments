9123fbf211d68a78309baded15232adb4ff3fa1b
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2019-11-15 10:32:48 -0500

    Week 5 Submit Button

---
+++ Assignments/FSW-105/Week 5/es6/index.js
@@ -0,0 +1,58 @@
+
+// let name = "John"
+// const age = 101
+
+// function runForLoop(pets) {
+//     const petObjects = []
+//     for (let i = 0; i < pets.length; i++) {
+//         let pet = { type: pets[i]}
+//         name;
+//         if (pets[i] === "cat") {
+//             name = "fluffy"
+//         } else {
+//             name = "spot"
+//         }
+//         console.log("pet name: ", name)
+//         pet.name = name
+//         //petObjects.push(pet)
+//     }
+//     console.log("man name: ", name)
+//     return petObjects
+// }
+// runForLoop(["cat", "dog"])
+
+// const carrots = ["bright orange", "ripe", "rotten"]
+
+// function mapVegetables(arr) {
+//     return arr.map(function(carrot) {
+//         return {type: "carrot", name: carrot}
+//     })
+// }
+
+// const people = [
+//     {
+//         name: "Princess Peach",
+//         friendly: false
+//     },
+//     {
+//         name: "Luigi",
+//         friendly: true
+//     },
+//     {
+//         name: "Mario",
+//         friendly: true
+//     },
+//     {
+//         name: "Bowser",
+//         friendly: false
+//     }
+// ]
+
+// const filterForFriendly = people.filter(person => person.friendly === true)
+// console.log(filterForFriendly)
+
+function doMathSum(a, b) {
+    return a + b
+}
+
+const produceProduct = function(a, b) => a * b
---
+++ Assignments/FSW-110/Week 5/submit_events/submit.js
@@ -0,0 +1,28 @@
+/*function firstButton (){
+    alert("You have clicked the first button!");
+}
+
+function secondButton (){
+    alert("Yay, I'm glad you clicked me!");
+}
+
+function thirdButton (){
+    alert("I knew you would click me!");
+}*/
+
+
+
+document.forms.addEventListener("submit", function(e){
+    e.preventDefault()
+
+    let firstName = document.forms.firstName.value
+    let secondName = document.forms.secondName.value
+    let age = document.forms.age.value
+    //let person = firstName + " " + secondName + " Age:" + age;
+    alert(firstName + " " + secondName + " Age: " + age)
+    document.getElementsByName("forms").reset()
+})
+
+function resetform() {
+    document.getElementsById("myForm").reset("myForm")
+}
