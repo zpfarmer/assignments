bb894f5e9d0bc7ccc535fa256098093db50e6dd4
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2019-11-24 19:57:12 -0500

    test

--- Assignments/FSW-105/Week 6/escape_room/index.js
+++ Assignments/FSW-105/Week 6/escape_room/index.js
@@ -1,9 +1,7 @@
 const { question } = require("readline-sync")
 
 let firstLine = question("You are stuck in a room! Type one to put hand in hole, two to find the key, or three to open the door")
-let one = 'one'
-let two = 'two'
-let three = 'three'
+
 
 getKey = () => {
     number = Math.floor((Math.random() * 10))
@@ -13,24 +11,23 @@ getKey = () => {
         return false
     }
 }    
+
+let one = "one"
+let two = "two"
+let three = "three"
 //console.log(getKey())
-const escapeRoom = (one, two, three) => {
-    if(firstLine == 'one'){
+const escapeRoom = () => {
+    if(firstLine == one){
         return "You have died!"
-    } else if(firstLine == 'two'){
+    } else if(firstLine == two){
         if(getKey() == true){
-            return "You have found the key!"
+            return question("You have found the key! What would you like to do with it?")
         } else if(getKey() == false){
             return "You couldn't find the key!"
         }
-    } else if(firstLine == 'three'){
+    } else if(firstLine == three){
         return "It is locked!"
     }}
 console.log(escapeRoom())
 
-const solution = () => {
-    if (escapeRoom = "You have found the key"){
-        return "you have escaped"
-    }
-}
-console.log(solution())
+//console.log(solution())
