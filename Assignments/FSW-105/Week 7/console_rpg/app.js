4905ad61e6dd1d94281ff8b6679702d8ce54ae3d
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2019-12-01 22:08:00 -0500

    FSW 105 Capstone

--- Assignments/FSW-105/Week 7/console_rpg/app.js
+++ Assignments/FSW-105/Week 7/console_rpg/app.js
@@ -3,8 +3,6 @@ const readlineSync = require('readline-sync');
 let endSimulation = false
 let name = readlineSync.question('Hello, what is your name?')
 
-//const choice = ['walk forward', 'check inventor', 'end sim']
-
 let inventory = {
     coins: 0,
     armor: 0,
@@ -15,27 +13,56 @@ let monster = {
     hp: 20
 }
 
- //console.log("Hello " + name + "please press 1 to walk, 2 to check your inventory, or 3 to end game")
 
- walk = () => {
+const choice = ['fight', 'run away']
+
+
+run = () => {
+    let runChance = Math.floor((Math.random() * 10))
+    if (runChance <= 5) {
+        console.log("You escaped! What would you like to do now?")
+    } else {
+        console.log("You did not escape and the monster killed you!")
+    }
+}
+
+fight = () => {
+    let winFight = Math.floor((Math.random() * 10)) 
+        if (winFight <= 5) {
+            console.log("You have defeated the monster!")
+        } else {
+            console.log("You have been slain by the monster!")
+        }
+}
+
+monsterAppears = () => {
+    
+    let indexOfChoice = readlineSync.keyInSelect(choice, "A monster has appeard! Would you like to fight it or run away?")
+        if (indexOfChoice === 0) {
+            return fight()
+        } else if (indexOfChoice === 1) {
+            return run()
+        }
+    }
+
+walk = () => {
      const number = Math.floor((Math.random() * 10))
      if (number < 3) {
-        console.log("A monster has appread!");
-        monsterAppears()
-     } else {
+        return monsterAppears();
+    } else {
          console.log("You have not found anything, please keep walking")
+         return endSimulation
      }
  }
 
- while (endSimulation === false) {
+  while (endSimulation === false) {
     let action = readlineSync.question("Hello " + name + " please press w to walk, 'print' to check your inventory, or x to end game")
     
     if (action === "w") {
-        return monsterAppears()
+        return walk()
     } else if (action === "print") {
     console.log(inventory)
     } else if (action === "x"){
         break
- }}
-
-monsterAppears
+    }
+  }
