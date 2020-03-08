c6e0f4e078f32130809e7c0fe3d337d390c20ca1
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2020-03-08 16:53:16 -0400

    Todo Express

--- Assignments/FSW-125/Week 4/Project 2 Todo Express/server.js
+++ Assignments/FSW-125/Week 4/Project 2 Todo Express/server.js
@@ -5,6 +5,8 @@ const uuid = require("uuid/v4")
 
 app.use(express.json())
 
+
+//array to start making a todo list
 const todo = [
     { 
     name: "My Name",
@@ -15,12 +17,20 @@ const todo = [
     }
 ]
 
-app.get("/:todo", (req, res) => {
-    const todoId = req.params.todoId
-    const foundTodo = todos.find(todo => todo._id === todoId)
-    res.setEncoding(foundTodo)
+//to perform a get request
+app.get("/todo", (req, res) => {
+    res.send(todo)
 })
 
+//to get one item from the database
+app.get("/:todo", (req, res) => {
+    const todoId = req.params.todoId
+    const foundTodo = todo.find(todo => todo._id === todoId)
+    res.set(foundTodo)
+})
+
+
+//to add an item to the database
 app.post("/todo", (req, res) => {
     const newTodo = req.body
     newTodo._id = uuid()
@@ -28,16 +38,21 @@ app.post("/todo", (req, res) => {
     res.send(`Successfully added ${newTodo.name} to the database`)
 })
 
+
+//to alter an item already in the database using its ID number
 app.put("/:todoId", (req, res) => {
     const todoId = req.params.todoId
+    const updateObject = req.body
     const todoIndex = todos.findIndex(todo = todo._id === todoId)
-    const updatedTodo = Object.assign(todos[todoIndex], req.body)
+    const updatedTodo = Object.assign(todos[todoIndex], updateObject)
     res.send(updatedTodo)
 })
 
+
+//to delete an item in the data base using its ID number
 app.delete("/:todoId", (req, res) => {
     const todoId = req.params.todoId
-    const todoIndex = todos.findIndex(todos => todo._id === todoId)
+    const todoIndex = todos.findIndex(todo => todo._id === todoId)
     todos.splice(todoIndex, 1)
     res.send("Successfully deleted Todo!")
 })
