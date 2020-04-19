c1d41ffdc9cacb460098fad432e77b0bb01eb6e4
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2020-04-19 17:18:11 -0400

    ecommerce part 2

---
+++ Assignments/FSW-135/week 2/ecommerce server-express-node/routes/inventoryRouter.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const inventoryRouter = express.Router()
+const Inventory = require('../models/inventory.js')
+
+inventoryRouter.get("/", (req, res, next) => {
+    Inventory.find((err, inventory) => {
+        if(err) {
+            res.state(500)
+            return next(err)
+        }
+        return res.status(200).send(inventory)
+    })
+})
+
+inventoryRouter.post("/", (req, res, next) => {
+    const newInventory = new Inventory(req.body)
+    newInventory.save((err, savedInventory) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedInventory)
+    })
+})
+
+inventoryRouter.delete("/:inventoryId", (req, res, next) => {
+    Inventory.findOneAndDelete({_id: req.params.inventoryId}, (err, deletedItem) => {
+        if(err){
+            res.status(500) 
+            return next(err)
+        }
+        return res.status(200).send(`Successfully deleted item ${deletedItem.title} from the database`) 
+    })
+})
+
+inventoryRouter.put("/:inventoryId", (req, res, next) => {
+    Inventory.findOneAndUpdate (
+    {_id: req.params.inventoryId } ,
+    req.body,
+    { new: true } ,
+    (err, updatedInventory) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(updatedInventory)
+    }  
+    )
+})
+
+module.exports = inventoryRouter
---
+++ Assignments/FSW-135/week 2/ecommerce server-express-node/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express() 
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+mongoose.connect('mongodb://localhost:27017/inventorydb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log("Connected to the DB")
+)
+
+app.use("/inventory", require("./routes/inventoryRouter.js"))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
+app.listen(9000, () => {
+    console.log("The server is running on Port 9000")
+})
