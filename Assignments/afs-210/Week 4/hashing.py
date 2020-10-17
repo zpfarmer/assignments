5cb0bf784997793556db64082ee52db2128cc115
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2020-10-17 15:14:37 -0400

    Project 2: Hashing

---
+++ Assignments/afs-210/Week 4/hashing.py
@@ -0,0 +1,8 @@
+#creating values to hash 
+int_val = 4
+str_val = 'LearningPython'
+flt_val = 24.56
+#hashing my above values to display in console
+print("The integer hash value is : " + str(hash(int_val)))
+print("The string hash value is : " + str(hash(str_val)))
+print("The float hash value is : " + str(hash(flt_val)))
