ab2bb43b7f246b660b15cc38a2ebd501a47c133f
zpfarmer <54371647+zpfarmer@users.noreply.github.com>
2020-10-25 23:14:15 -0400

    Week 5

---
+++ Assignments/afs-210/Week 5/mergesort.py
@@ -0,0 +1,32 @@
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    if len(nlist)>1:
+        mid = len(nlist)//2
+        lefthalf = nlist[:mid]
+        righthalf = nlist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=j=k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                nlist[k]=lefthalf[i]
+                i=i+1
+            else:
+                nlist[k]=lefthalf[i]
+                j=j+1
+
+        while i < len(lefthalf):
+            nlist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            nlist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ", nlist)
+
+nlist= [14, 21, 43, 46, 70, 27, 41, 45, 57]
+mergeSort(nlist)
+print(nlist)
